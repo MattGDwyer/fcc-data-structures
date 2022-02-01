cedc394b0870fa5fd2e01e2d3272a2efd1c69808
Matthew Dwyer <matthewgdwyer@gmail.com>
2022-02-01 17:28:23 +0700

    Set completed

--- dataStructuresOne.js
+++ dataStructuresOne.js
@@ -162,3 +162,52 @@ class CircularQueue {
     // Only change code above this line
   }
 }
+
+// 7. First, we will create an add method that adds a value to our set collection as long as the value does not already exist in the set. Then we will create a remove method that removes a value from the set collection if it already exists. And finally, we will create a size method that returns the number of elements inside the set collection.
+
+// Create an add method that adds a unique value to the set collection and returns true if the value was successfully added and false otherwise.
+
+// Create a remove method that accepts a value and checks if it exists in the set. If it does, then this method should remove it from the set collection, and return true. Otherwise, it should return false. Create a size method that returns the size of the set collection.
+
+class Set {
+  constructor() {
+    // Dictionary will hold the items of our set
+    this.dictionary = {};
+    this.length = 0;
+  }
+
+  // This method will check for the presence of an element and return true or false
+  has(element) {
+    return this.dictionary[element] !== undefined;
+  }
+
+  // This method will return all the values in the set
+  values() {
+    return Object.values(this.dictionary);
+  }
+
+  // Only change code below this line
+  add(item) {
+    if (this.has(item)) {
+      return false;
+    } else {
+      this.dictionary[item] = item;
+      this.length++;
+      return true;
+    }
+  }
+
+  remove(value) {
+    if (this.has(value)) {
+      this.length--;
+      delete this.dictionary[value];
+      return true;
+    }
+    return false;
+  }
+  size() {
+    // console.log('poo', this.length);
+    return this.length;
+  }
+  // Only change code above this line
+}
