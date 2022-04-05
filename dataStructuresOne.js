e535bcab08deddde54f40e015cf3d8e6b527f114
Matthew Dwyer <matthewgdwyer@gmail.com>
2022-04-05 10:34:20 +0700

    ES6 set complete

--- dataStructuresOne.js
+++ dataStructuresOne.js
@@ -524,3 +524,20 @@ class Set {
   }
   // Only change code above this line
 }
+
+/**
+ * 12. For this exercise, return a set with the following values: 1, 2, 3, 'Taco', 'Cat', 'Awesome'
+ */
+
+ function checkSet() {
+  var set = new Set([1, 2, 3, 3, 2, 1, 2, 3, 1]);
+  // Only change code below this line
+  set.add('Taco');
+  set.add('Cat');
+  set.add('Awesome');
+  // Only change code above this line
+  console.log(Array.from(set));
+  return set;
+}
+
+checkSet();
