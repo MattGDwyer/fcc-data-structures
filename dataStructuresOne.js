79cc6ececb74032b499b0bc854b216d8fbf77e20
Matthew Dwyer <matthewgdwyer@gmail.com>
2022-04-22 10:21:23 +0700

    Linked List problem complete

--- dataStructuresOne.js
+++ dataStructuresOne.js
@@ -751,3 +751,43 @@ let Dog = new Node('Dog');
 
 Puppy.next = Cat;
 Cat.next = Dog;
+
+// 20. Write an add method that assigns the first node you push to the linked list to the head; after that, whenever adding a node, every node should be referenced by the previous node's next property.
+
+// Note
+
+// Your list's length should increase by one every time an element is added to the linked list.
+
+function LinkedList() {
+  var length = 0;
+  var head = null;
+
+  var Node = function(element){
+    this.element = element;
+    this.next = null;
+  };
+
+  this.head = function(){
+    return head;
+  };
+
+  this.size = function(){
+    return length;
+  };
+
+  this.add = function(element){
+    // Only change code below this line
+
+    if (head === null) {
+      head =new Node(element);
+    } else {
+      let currentNode = head;
+      while (currentNode.next !== null) {
+        currentNode = currentNode.next;
+      }
+      currentNode.next = new Node(element);
+    }
+    length++;
+    // Only change code above this line
+  };
+}
