85789aa2fb3921750389f123463c80c923c3cf1b
Matthew Dwyer <matthewgdwyer@gmail.com>
2022-04-25 10:37:37 +0700

    linked list remove problem complete

--- dataStructuresOne.js
+++ dataStructuresOne.js
@@ -791,3 +791,67 @@ function LinkedList() {
     // Only change code above this line
   };
 }
+
+// 21. Write a remove method that takes an element and removes it from the linked list.
+
+// Note: The length of the list should decrease by one every time an element is removed from the linked list.
+
+function LinkedList() {
+  var length = 0;
+  var head = null;
+
+  var Node = function(element){
+    this.element = element;
+    this.next = null;
+  };
+
+  this.size = function(){
+    return length;
+  };
+
+  this.head = function(){
+    return head;
+  };
+
+  this.add = function(element){
+    var node = new Node(element);
+    if(head === null){
+        head = node;
+    } else {
+      var currentNode = head;
+
+      while(currentNode.next){
+        currentNode  = currentNode.next;
+      }
+
+      currentNode.next = node;
+    }
+
+    length++;
+  };
+
+  this.remove = function(element){
+    // Only change code below this line
+    let node = new Node(element);
+    if (node.element === head.element) {
+      let newHead = head.next;
+      head = newHead;
+      length--;
+
+    } else {
+      let currentNode = head;
+
+      while (currentNode.next) {
+        let previousNode = currentNode;
+        currentNode = currentNode.next;
+
+        if (currentNode.element === node.element) {
+          let newLink = currentNode.next;
+          console.log(previousNode.next);
+          previousNode.next = newLink;
+        }
+      }
+    }
+    // Only change code above this line
+  };
+}
